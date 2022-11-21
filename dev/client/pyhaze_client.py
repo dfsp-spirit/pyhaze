490275128ae19527006f64819fe6a22174cf9739
Tim Schäfer <ts+code@rcmd.org>
2022-11-21 21:01:08 +0100

    NEW: finish demo client

--- dev/client/pyhaze_client.py
+++ dev/client/pyhaze_client.py
@@ -9,7 +9,17 @@ import nibabel.freesurfer.io as fsio
 import warnings
 
 def pyhazecl():
-    parser = argparse.ArgumentParser(description="View brain morphometry data.")
+
+     # Parse command line arguments
+    example_text = '''
+    Run with '--help' to get help.
+ '''
+
+    parser = argparse.ArgumentParser(prog="pyhaze_client",
+                                     description="Smooth per-vertex data on triangular meshes.",
+                                     epilog=example_text,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter
+                                     )
     parser.add_argument("pvdfile", help="str, the file containg the input per-vertex data.")
     parser.add_argument("meshfile", help="str, the mesh file for the input data.")
     parser.add_argument("-o", "--outputfile", help="str, output file name for smoothed per-vertex data. Defaults to 'smoothed.pvd'", default="smoothed.pvd")
@@ -29,11 +39,18 @@ def pyhazecl():
     warn_igl = not args.no_warn_igl
     silent = args.silent
 
+    if not silent:
+        print(f"Smoothing pvd data from file '{pvdfile}' on mesh '{meshfile}' with {num_iter} iterations.")
+
     if not os.path.isfile(pvdfile):
         raise ValueError(f"Cannot read input per-vertex data file '{pvdfile}'.")
 
     if not os.path.isfile(meshfile):
         raise ValueError(f"Cannot read input mesh file '{meshfile}'.")
+
+    if meshfile == pvdfile:
+        raise ValueError(f"The files passed as mesh file '{meshfile}' and per-vertex data file '{pvdfile}' cannot be identical.")
+
 
     vertices, faces = fsio.read_geometry(meshfile)
     pvd_data = fsio.read_morph_data(pvdfile)
@@ -41,7 +58,7 @@ def pyhazecl():
     assert vertices.shape[0] == np.size(pvd_data), f"Length of input per-vertex data {np.size(pvd_data)} does not match number of mesh vertices {vertices.shape[0]}."
 
     if not silent:
-        print(f"Read mesh with {vertices.shape[0]} vertices and suitable per-vertex data.")
+        print(f" - Read mesh with {vertices.shape[0]} vertices and suitable per-vertex data.")
 
     has_igl = False
     try:
@@ -54,16 +71,16 @@ def pyhazecl():
     if has_igl:
         faces_igl = np.array(faces).reshape(-1, 3).astype(np.int64)
         mesh_adj = igl.adjacency_list(faces_igl)
-        res = pyhaze.smooth_pvd_adj(mesh_adj, pvd_data.tolist(), num_iter) # TODO: use with_nan and via_matrix
+        res = pyhaze.smooth_pvd_adj(mesh_adj, pvd_data.tolist(), num_iter=num_iter, with_nan=with_nan)
     else:
-        res = pyhaze.smooth_pvd(vertices, faces, pvd_data.tolist(), num_iter) # TODO: use with_nan and via_matrix
+        res = pyhaze.smooth_pvd(vertices, faces, pvd_data.tolist(), num_iter=num_iter, via_matrix=via_matrix, with_nan=with_nan)
 
     assert np.size(res) == np.size(pvd_data), f"Length of smoothed per-vertex data {np.size(res)} does not match number of mesh vertices {vertices.shape[0]}."
 
     fsio.write_morph_data(outfile, res)
 
     if not silent:
-        print(f"Smoothed per-vertex data for {vertices.shape[0]} vertices written to output file '{outfile}'.")
+        print(f" - Smoothed per-vertex data for {vertices.shape[0]} vertices written to output file '{outfile}'.")
 
 if __name__ == "__main__":
     pyhazecl()
--- python/pyhaze_python.cpp
+++ python/pyhaze_python.cpp
@@ -25,68 +25,69 @@ inline py::array_t<typename Sequence::value_type> as_pyarray(Sequence &&seq) {
 namespace pyhaze {
 
 
-/** @brief Return a simple cube mesh in vertex index list representation.
- * @returns : Tuple of 2D np.ndarrays, the first ndarray contains the nx3 vertex coordinates, the second ndarray contains the mx3 triangles (faces).
- */
-static py::tuple construct_cube() {
-  fs::Mesh m = fs::Mesh::construct_cube();
-  return py::make_tuple(std::move(fs::util::v2d(m.vertices, 3)), std::move(fs::util::v2d(m.faces, 3)));
-}
+  /** @brief Return a simple cube mesh in vertex index list representation.
+   * @returns : Tuple of 2D np.ndarrays, the first ndarray contains the nx3 vertex coordinates, the second ndarray contains the mx3 triangles (faces).
+   */
+  static py::tuple construct_cube() {
+    fs::Mesh m = fs::Mesh::construct_cube();
+    return py::make_tuple(std::move(fs::util::v2d(m.vertices, 3)), std::move(fs::util::v2d(m.faces, 3)));
+  }
 
 
-/** @brief Smooth per-vertex data on a mesh given as adjacency list using nearest edge neighbor smoothing and uniform weights.
- *
- * @param mesh_adj : 2d array of integers, adjacency list representation of the faces of a mesh. The outer array has size num_vertices, the length of the inner arrays are the number of neighbors of the respective vertex.
- * @param pvd      : 1d array of floats, the per-vertex data for the mesh, with length num_vertices.
- * @param num_iter : scalar int, the number of iterations of nearest neighbor smoothing to apply.
- * @param with_nan : bool, whether to enable NAN support. On by default. Set to off for small speed boost if you are sure you have no NAN values.
- * @returns        : 1d numpy array of floats, the smoothed per-vertex data for the mesh, with length num_vertices.
- */
-static py::array_t<float> smooth_pvd_adj(const std::vector<std::vector<size_t>> mesh_adj, const std::vector<float> pvd, const size_t num_iter, const bool with_nan=true) {
-  auto temp_vector = smooth_pvd_nn_cpp_adj(mesh_adj, pvd, num_iter, with_nan);
-  return as_pyarray(std::move(temp_vector));
-}
+  /** @brief Smooth per-vertex data on a mesh given as adjacency list using nearest edge neighbor smoothing and uniform weights.
+   *
+   * @param mesh_adj : 2d array of integers, adjacency list representation of the faces of a mesh. The outer array has size num_vertices, the length of the inner arrays are the number of neighbors of the respective vertex.
+   * @param pvd      : 1d array of floats, the per-vertex data for the mesh, with length num_vertices.
+   * @param num_iter : scalar int, the number of iterations of nearest neighbor smoothing to apply.
+   * @param with_nan : bool, whether to enable NAN support. On by default. Set to off for small speed boost if you are sure you have no NAN values.
+   * @returns        : 1d numpy array of floats, the smoothed per-vertex data for the mesh, with length num_vertices.
+   */
+  static py::array_t<float> smooth_pvd_adj(const std::vector<std::vector<size_t>> mesh_adj, const std::vector<float> pvd, const size_t num_iter, const bool with_nan=true) {
+    auto temp_vector = smooth_pvd_nn_cpp_adj(mesh_adj, pvd, num_iter, with_nan);
+    return as_pyarray(std::move(temp_vector));
+  }
 
-/** @brief Smooth per-vertex data on a mesh given as a vertex index list using nearest edge neighbor smoothing and uniform weights.
- *
- * @param mesh_vertices : 2d array of floats, the vertex coordinates for N vertices as an Nx3 matrix. Can be empty, as vertex coordinates are not required for nearest neighbor smoothing. In here to prevent calling this with an adjacency list by accident.
- * @param mesh_faces    : 2d array of size_t, the faces for N vertices as an Nx3 matrix of vertex indices.
- * @param pvd           : 1d array of floats, the per-vertex data for the mesh, with length num_vertices.
- * @param num_iter      : scalar int, the number of iterations of nearest neighbor smoothing to apply.
- * @param via_matrix    : whether the mesh adjacency list should be computed via a matrix (faster, requires more memory) as opposed to an edge list (slower, requires less memory).
- * @param with_nan      : bool, whether to enable NAN support. On by default. Set to off for small speed boost if you are sure you have no NAN values.
- * @returns             : 1d numpy array of floats, the smoothed per-vertex data for the mesh, with length num_vertices.
- */
-static py::array_t<float> smooth_pvd(const std::vector<std::vector<float>> mesh_vertices, const std::vector<std::vector<int32_t>> mesh_faces, const std::vector<float> pvd, const size_t num_iter = 1, const bool via_matrix = true, const bool with_nan=true) {
-  fs::Mesh m = fs::Mesh(mesh_vertices, mesh_faces);
-  auto mesh_adj = m.as_adjlist(via_matrix);
-  auto temp_vector = smooth_pvd_nn_cpp_adj(mesh_adj, pvd, num_iter, with_nan);
-  return as_pyarray(std::move(temp_vector));
-}
+  /** @brief Smooth per-vertex data on a mesh given as a vertex index list using nearest edge neighbor smoothing and uniform weights.
+   *
+   * @param mesh_vertices : 2d array of floats, the vertex coordinates for N vertices as an Nx3 matrix. Can be empty, as vertex coordinates are not required for nearest neighbor smoothing. In here to prevent calling this with an adjacency list by accident.
+   * @param mesh_faces    : 2d array of size_t, the faces for N vertices as an Nx3 matrix of vertex indices.
+   * @param pvd           : 1d array of floats, the per-vertex data for the mesh, with length num_vertices.
+   * @param num_iter      : scalar int, the number of iterations of nearest neighbor smoothing to apply.
+   * @param via_matrix    : whether the mesh adjacency list should be computed via a matrix (faster, requires more memory) as opposed to an edge list (slower, requires less memory).
+   * @param with_nan      : bool, whether to enable NAN support. On by default. Set to off for small speed boost if you are sure you have no NAN values.
+   * @returns             : 1d numpy array of floats, the smoothed per-vertex data for the mesh, with length num_vertices.
+   */
+  static py::array_t<float> smooth_pvd(const std::vector<std::vector<float>> mesh_vertices, const std::vector<std::vector<int32_t>> mesh_faces, const std::vector<float> pvd, const size_t num_iter = 1, const bool via_matrix = true, const bool with_nan=true) {
+    fs::Mesh m = fs::Mesh(mesh_vertices, mesh_faces);
+    auto mesh_adj = m.as_adjlist(via_matrix);
+    auto temp_vector = smooth_pvd_nn_cpp_adj(mesh_adj, pvd, num_iter, with_nan);
+    return as_pyarray(std::move(temp_vector));
+  }
 
-// See doSomethingWithArray function in
-// https://developer.lsst.io/v/u-ktl-debug-fix/coding/python_wrappers_for_cpp_with_pybind11.html
-// for an example of a C++ function that takes a np.ndarray as argument.
+  // See doSomethingWithArray function in
+  // https://developer.lsst.io/v/u-ktl-debug-fix/coding/python_wrappers_for_cpp_with_pybind11.html
+  // for an example of a C++ function that takes a np.ndarray as argument.
 
 
-PYBIND11_MODULE(pyhaze, m) {
-  m.doc() = "Python Bindings for pyhaze";
+  PYBIND11_MODULE(pyhaze, m) {
+    m.doc() = "Python Bindings for pyhaze";
 
-  py::class_<fs::Mesh>(m, "Mesh")
-        .def(py::init<std::vector<float>, std::vector<int>>())
-        .def(py::init<std::vector<std::vector<float>>, std::vector<std::vector<int>>>())
-        .def("to_obj", &fs::Mesh::to_obj)
-        .def("as_adjlist", &fs::Mesh::as_adjlist)
-        .def("num_vertices", &fs::Mesh::num_vertices)
-        .def("num_faces", &fs::Mesh::num_faces)
-        .def_readwrite("vertices", &fs::Mesh::vertices)
-        .def_readwrite("faces", &fs::Mesh::faces)
-        .def("as_edgelist", &fs::Mesh::as_edgelist);
+    py::class_<fs::Mesh>(m, "Mesh")
+          .def(py::init<std::vector<float>, std::vector<int>>())
+          .def(py::init<std::vector<std::vector<float>>, std::vector<std::vector<int>>>())
+          .def("to_obj", &fs::Mesh::to_obj)
+          .def("to_ply", &fs::Mesh::to_ply)
+          .def("to_off", &fs::Mesh::to_off)
+          .def("as_adjlist", &fs::Mesh::as_adjlist)
+          .def("num_vertices", &fs::Mesh::num_vertices)
+          .def("num_faces", &fs::Mesh::num_faces)
+          .def_readwrite("vertices", &fs::Mesh::vertices)
+          .def_readwrite("faces", &fs::Mesh::faces)
+          .def("as_edgelist", &fs::Mesh::as_edgelist);
 
-  m.def("construct_cube", &construct_cube, "Construct simple cube mesh.");
-  m.def("smooth_pvd_adj", &smooth_pvd_adj, "Smooth data on mesh given as adjacency list. Allows you to use fast methods to computes mesh adjacency, like igl.adjacency_list from the 'igl' Python package.", py::arg("mesh_adj"), py::arg("pvd"), py::arg("num_iter") = 1, py::arg("with_nan") = true);
-  m.def("smooth_pvd", &smooth_pvd, "Smooth data on mesh given as vertex index list.", py::arg("mesh_vertices"), py::arg("mesh_faces"), py::arg("pvd"), py::arg("num_iter") = 1, py::arg("via_matrix") = true, py::arg("with_nan") = true);
-
+    m.def("construct_cube", &construct_cube, "Construct simple cube mesh.");
+    m.def("smooth_pvd_adj", &smooth_pvd_adj, "Smooth data on mesh given as adjacency list. Allows you to use fast methods to compute mesh adjacency, like `igl.adjacency_list()` from the 'igl' Python package.", py::arg("mesh_adj"), py::arg("pvd"), py::arg("num_iter") = 1, py::arg("with_nan") = true);
+    m.def("smooth_pvd", &smooth_pvd, "Smooth data on mesh given as vertex index list.", py::arg("mesh_vertices"), py::arg("mesh_faces"), py::arg("pvd"), py::arg("num_iter") = 1, py::arg("via_matrix") = true, py::arg("with_nan") = true);
   } // PYBIND11_MODULE
 
 } // namespace pyhaze
