a25773eaf83e6f93118c9343aefd92f9b02ae9a2
Tim Schaefer <ts+code@rcmd.org>
2022-11-22 13:33:59 +0100

    NEW: add WIP to extend adj

--- python/pyhaze_python.cpp
+++ python/pyhaze_python.cpp
@@ -4,6 +4,9 @@
 
 #include "pyhaze/pyhaze.hpp"
 
+#include <algorithm>
+#include <vector>
+
 namespace py = pybind11;
 
 // helper function to avoid making a copy when returning a py::array_t
@@ -47,6 +50,30 @@ namespace pyhaze {
     return as_pyarray(std::move(temp_vector));
   }
 
+  std::vector<std::vector<size_t>> extend_adj(const std::vector<std::vector<size_t>> mesh_adj, const size_t extend_by=1) {
+    size_t num_vertices = mesh_adj.size();
+    std::vector<std::vector<size_t>> mesh_adj_ext = mesh_adj;
+    std::vector<size_t> neighborhood;
+    std::vector<size_t> ext_neighborhood;
+    for(size_t ext_idx = 0; ext_idx < extend_by; ext_idx++) {
+      for(size_t source_vert_idx = 0; source_vert_idx < num_vertices; source_vert_idx++) {
+        neighborhood = mesh_adj_ext[source_vert_idx];
+        // Extension: add all neighbors in distance one for all vertices in the neighborhood.
+        for(size_t neigh_vert_rel_idx = 0; neigh_vert_rel_idx < neighborhood.size(); neigh_vert_rel_idx++) {
+          for(size_t canidate_rel_idx = 0; canidate_rel_idx < mesh_adj[neighborhood[neigh_vert_rel_idx]].size(); canidate_rel_idx++) {
+            if(mesh_adj[neighborhood[neigh_vert_rel_idx]][canidate_rel_idx] != source_vert_idx) {
+              mesh_adj_ext[source_vert_idx].push_back(mesh_adj[neighborhood[neigh_vert_rel_idx]][canidate_rel_idx]);
+            }
+          }
+        }
+        // We need to remove duplicates.
+        std::sort(mesh_adj_ext[source_vert_idx].begin(), mesh_adj_ext[source_vert_idx].end());
+        mesh_adj_ext[source_vert_idx].erase(std::unique(mesh_adj_ext[source_vert_idx].begin(), mesh_adj_ext[source_vert_idx].end()), mesh_adj_ext[source_vert_idx].end());
+      }
+    }
+    return mesh_adj_ext;
+  }
+
   /** @brief Smooth per-vertex data on a mesh given as a vertex index list using nearest edge neighbor smoothing and uniform weights.
    *
    * @param mesh_vertices : 2d array of floats, the vertex coordinates for N vertices as an Nx3 matrix. Can be empty, as vertex coordinates are not required for nearest neighbor smoothing. In here to prevent calling this with an adjacency list by accident.
@@ -86,6 +113,7 @@ namespace pyhaze {
     m.def("construct_cube", &construct_cube, "Construct simple cube mesh.");
     m.def("smooth_pvd_adj", &smooth_pvd_adj, "Smooth data on mesh given as adjacency list. Allows you to use fast methods to compute mesh adjacency, like `igl.adjacency_list()` from the 'igl' Python package.", py::arg("mesh_adj"), py::arg("pvd"), py::arg("num_iter") = 1, py::arg("with_nan") = true);
     m.def("smooth_pvd", &smooth_pvd, "Smooth data on mesh given as vertex index list.", py::arg("mesh_vertices"), py::arg("mesh_faces"), py::arg("pvd"), py::arg("num_iter") = 1, py::arg("via_matrix") = true, py::arg("with_nan") = true);
+    m.def("extend_adj", &extend_adj, "Extend neighborhoods by k iterations of edge hopping.", py::arg("mesh_adj"), py::arg("extend_by") = 1);
   } // PYBIND11_MODULE
 
 } // namespace pyhaze
